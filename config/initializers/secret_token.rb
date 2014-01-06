0a7e1faedf4a492e7bf1736bd365c7781b9e765f
LDCONQ <ldconq@ldconq-VirtualBox.(none)>
2014-01-05 17:26:26 -1000

    my 2nd rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embeddr::Application.config.secret_key_base = '75db43975c46f9ae6f2ddf8cb7787cbb1aaa84b9ba33d3b0fbe94ef7918c5efd14d8edfcbcb6f5044f0d15a26f9477d9d9e9ee44ec461cf8773264d375da78b1'
